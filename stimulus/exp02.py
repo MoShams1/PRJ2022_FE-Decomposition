4edfe75acbb16a652cb7a71098706d50fd819e59
kvnl <m.shamsahmar@gmail.com>
2024-03-12 18:04:38 -0400

    recording finished on Tuesday

--- stimulus/exp02.py
+++ stimulus/exp02.py
@@ -24,7 +24,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # -------------------------------------------------
 # insert session meta data
 # -------------------------------------------------
-person = 'AM'
+person = 'HW'
 n_trials = 30
 # -------------------------------------------------
 # destination file
