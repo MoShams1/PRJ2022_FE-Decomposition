a346c01b8f1ba3e852ecc4fb6aaba380b4708fe6
kvnl <m.shamsahmar@gmail.com>
2024-03-17 12:35:35 -0400

    recording AR

--- stimulus/exp02.py
+++ stimulus/exp02.py
@@ -24,7 +24,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # -------------------------------------------------
 # insert session meta data
 # -------------------------------------------------
-person = 'HW'
+person = 'AR'
 n_trials = 30
 # -------------------------------------------------
 # destination file
