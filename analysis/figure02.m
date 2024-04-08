ace104b51d01f33f395bf571d7a065c7d21c52d6
Mohammad Shams <m.shamsahmar@gmail.com>
2024-04-08 19:07:58 -0400

    new analyses

--- analysis/figure02.m
+++ analysis/figure02.m
@@ -7,6 +7,7 @@ close all
 file_dir = dir('../data/*Exp01*');
 nsub = numel(file_dir);
 
+click_coeff = 4;
 
 for isub = 1:nsub
 
@@ -25,10 +26,10 @@ for isub = 1:nsub
     %%% convert structure to arrays
     cnd_proben = struct2cell(jsonData.cnd);
     cnd_double = strcmp(cnd_proben,'double');
-    click1_x = cell2mat(struct2cell(jsonData.click1_xloc));
+    click1_x = cell2mat(struct2cell(jsonData.click1_xloc)) * click_coeff;
     click2_x = struct2cell(jsonData.click2_xloc);
     click2_x(cellfun(@isempty, click2_x)) = {NaN};
-    click2_x = cell2mat(click2_x);
+    click2_x = cell2mat(click2_x) * click_coeff;
     probe_x = cell2mat(struct2cell(jsonData.probe_xloc));
     cnd_cycn = cell2mat(struct2cell(jsonData.frame_ncycles));
 
@@ -53,17 +54,18 @@ for isub = 1:nsub
 end
 
 %%% =====================================================================================
-%%%%% Figure 2A
+%%%%% Figure 2B
 
-figure('units','inches','outerposition',[7, 4, 3, 5])
-
+figure('units','inches','outerposition',[7, 4, 5, 5])
+subplot(1,2,1)
 hold on
+
 cmap = lines(7);
-csingle = 'k';
-cdouble = cmap(7,:);
+csingle = [0 0 0];
+cdouble = cmap(5,:) * .85;
 lw = 1.5;
 xtick_vec = 1:3;
-ytick_vec = 0:1.5;
+ytick_vec = 0:4;
 marker_sz = 50;
 marker_a = .2;
 alpha = .2;
@@ -76,17 +78,17 @@ scatter( ...
     marker_sz, csingle, 'o', 'filled', 'markerfacealpha', alpha);
 scatter( ...
     xmat, two_probe_err_mat, ...
-    marker_sz*1.5, cdouble, 's', 'filled', 'markerfacealpha', alpha);
+    marker_sz, cdouble, 'o', 'filled', 'markerfacealpha', alpha);
 
 errorbar(...
-    x, nanmean(one_probe_err_mat), SE(one_probe_err_mat),...
+    x, nanmedian(one_probe_err_mat), SE(one_probe_err_mat),...
     'color',csingle,...
     'linewidth',lw,...
     'marker','o',...
     'markerfacecolor',csingle,...
     'markeredgecolor',csingle)
 errorbar(...
-    x, nanmean(two_probe_err_mat), SE(two_probe_err_mat),...
+    x, nanmedian(two_probe_err_mat), SE(two_probe_err_mat),...
     'color',cdouble,...
     'linewidth',lw,...
     'marker','s',...
@@ -97,25 +99,130 @@ xlabel 'Number of motion cycles'
 xticks(xtick_vec)
 xlim([xtick_vec(1)-.5 xtick_vec(end)+.5])
 
-ylabel 'Perceived offset (dva)'
+ylabel 'Click offset (dva)'
 yticks(ytick_vec)
-ylim([ytick_vec(1)-.5 ytick_vec(end)+.5])
+ylim([ytick_vec(1)-.5 ytick_vec(end)])
 
 yline(0, 'linestyle','--')
 
-text(1, 1.4, 'One-probe', 'color', csingle, 'horizontalalignment','center')
-text(1, 1.3, 'Two-probe', 'color', cdouble, 'horizontalalignment','center')
+text(1, .5, 'One-probe', 'color', csingle, 'horizontalalignment','left')
+text(1, 0.25, 'Two-probe', 'color', cdouble, 'horizontalalignment','left')
 text(3, -.25, ['N = ', num2str(nsub)])
 
 cleanplot
 
-saveas(gcf, '../results/fig02A.pdf')
+%%% ---------------------------------
+%%% Stat fig 2B
+
+disp('----- Fig 2B -----')
+disp(' ')
+
+[p1, stats1] = friedman(one_probe_err_mat,1,'off');
+[p2, stats2] = friedman(two_probe_err_mat,1,'off');
+
+fprintf('<One-probe> Chi-sq(%d, %d)=%6.3f, p=%5.3f \n', stats1{2,3}, nsub, stats1{2,5}, stats1{2,6})
+fprintf('<Two-probe> Chi-sq(%d, %d)=%6.3f, p=%5.3f \n', stats2{2,3}, nsub, stats2{2,5}, stats2{2,6})
+
+d31 = median(two_probe_err_mat(:,3)-two_probe_err_mat(:,1));
+d32 = median(two_probe_err_mat(:,3)-two_probe_err_mat(:,2));
+d21 = median(two_probe_err_mat(:,2)-two_probe_err_mat(:,1));
+
+p31 = signrank(two_probe_err_mat(:,3), two_probe_err_mat(:,1));
+p32 = signrank(two_probe_err_mat(:,3), two_probe_err_mat(:,2));
+p21 = signrank(two_probe_err_mat(:,2), two_probe_err_mat(:,1));
+
+[med, p, W, z, r] = signrank_full(two_probe_err_mat(:,3), two_probe_err_mat(:,1));
+fprintf('<3 vs 1> md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
+
+[med, p, W, z, r] = signrank_full(two_probe_err_mat(:,3), two_probe_err_mat(:,2));
+fprintf('<3 vs 2> md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
+
+[med, p, W, z, r] = signrank_full(two_probe_err_mat(:,2), two_probe_err_mat(:,1));
+fprintf('<2 vs 1> md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n\n', ...
+med,W,z,p,r)
+
+
+line([1 2], [3.25 3.25], 'color', cdouble, 'linewidth', 2)
+text(1.5, 3.45, '\it n.s.', 'color', cdouble, 'horizontalalignment','center')
+line([2 3], [3.4 3.4], 'color', cdouble, 'linewidth', 2)
+text(2.5, 3.5, '***', 'fontsize', 18, 'color', cdouble, 'horizontalalignment','center')
+line([1 3], [3.75 3.75], 'color', cdouble, 'linewidth', 2)
+text(2, 3.85, '*', 'fontsize', 18, 'color', cdouble, 'horizontalalignment','center')
+
+%%% =====================================================================================
+%%%%% Figure 2C
+
+one_probe_err_pooled = mean(one_probe_err_mat,2);
+two_probe_err_pooled = mean(two_probe_err_mat,2);
+
+subplot(1,2,2)
+hold on
+
+y = {one_probe_err_pooled, two_probe_err_pooled};
+
+xs = scatterbar(y, 50, [csingle; cdouble]);
+plot(xs', [one_probe_err_pooled, two_probe_err_pooled]','color',[.5 .5 .5])
+scatterbar(y, 50, [csingle; cdouble]);
+
+yline(0, 'linestyle','--')
+
+xticks([1 2])
+xticklabels({'One-probe', 'Two-probe'})
+xlim([.5 2.5])
+
+ylabel 'Click offset (dva)'
+yticks(ytick_vec)
+ylim([ytick_vec(1)-.5 ytick_vec(end)])
+
+cleanplot
 
 %%% ---------------------------------
-%%% Stat fig 2A
+%%% Stat fig 2B
 
-% display(['Fig02A, Two-probe (anova-like test): p = ', num2str(p_boot)])
+disp('----- Fig 2C -----')
 
-% display(['Fig02A, One-probe (anova-like test): p = ', num2str(p_boot)])
+[med, p, W, z, r] = signrank_full(two_probe_err_pooled, one_probe_err_pooled);
+fprintf('\n<One-probe vs Two-probe> md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
 
+perc = median((two_probe_err_pooled-one_probe_err_pooled) ./ two_probe_err_pooled * 100);
 
+fprintf('delta in percentage: %3d%% \n', round(perc))
+
+line([1 2], [3.5 3.5], 'color', 'k', 'linewidth', 2)
+text(1.5, 3.6, '***', 'fontsize', 18, 'horizontalalignment','center')
+
+%%% =====================================================================================
+saveas(gcf, '../results/fig02BC.pdf')
+
+%%% =====================================================================================
+% scatterbar 1.0
+% Mohammad Shams <m.shamsahmar@gmail.com>
+% Created: Apr 1, 2019
+% Modified: Mar 20, 2024
+
+function xs = scatterbar(A,marksz,c)
+% A: a cell of cetegories
+
+ncat    = numel(A); % number of categories
+stdx    = .07; % standard deviation of scatters in each category
+linelm  = .4; % line length for median
+
+hold on
+for icat = 1:ncat    
+    rng default
+    n = numel(A{icat});
+    x = randn(n,1)*stdx + icat;
+    xs(:,icat) = x;
+    
+    scatter(x,A{icat}, ...
+        marksz, c(icat,:), 'o', 'filled', 'markerfacealpha', .4);
+    line([icat-linelm icat+linelm],[nanmedian(A{icat}) nanmedian(A{icat})],...
+        'color',c(icat,:),'linewidth',1);
+end
+
+xlim([0 ncat+1])
+set(gca,'xtick',1:ncat)
+end
--- analysis/figure03.m
+++ analysis/figure03.m
@@ -7,6 +7,7 @@ close all
 file_dir = dir('../data/*Exp02*');
 nsub = numel(file_dir);
 
+click_coeff = 4;
 
 for isub = 1:nsub
 
@@ -26,10 +27,10 @@ for isub = 1:nsub
     cnd_double = strcmp(cnd_proben,'double');
     cnd_single1 = strcmp(cnd_proben,'single1');
     cnd_single2 = strcmp(cnd_proben,'single2');
-    click1_x = cell2mat(struct2cell(jsonData.click1_xloc));
+    click1_x = cell2mat(struct2cell(jsonData.click1_xloc)) * click_coeff;
     click2_x = struct2cell(jsonData.click2_xloc);
     click2_x(cellfun(@isempty, click2_x)) = {NaN};
-    click2_x = cell2mat(click2_x);
+    click2_x = cell2mat(click2_x) * click_coeff;
     probe_x = cell2mat(struct2cell(jsonData.probe_xloc));
     frame_dir = struct2cell(jsonData.frame_dir);
     dir_left = strcmp(frame_dir,'left');
@@ -44,11 +45,11 @@ for isub = 1:nsub
     click2_err_vec = click2_x - probe_x;
     click2_err_vec(dir_left) = -click2_err_vec(dir_left);
 
-    lead2_vec = max([click1_err_vec, click2_err_vec], [], 2);
+    lead2_vec = click1_err_vec;  % as the subjs had to click in order of appearance
     lead2_vec(~cnd_double) = nan;
     lead2(isub,1) = nanmean(lead2_vec);
 
-    trail2_vec = min([click1_err_vec, click2_err_vec], [], 2);
+    trail2_vec = click2_err_vec;
     trail2_vec(~cnd_double) = nan;
     trail2(isub,1) = nanmean(trail2_vec);
 
@@ -68,117 +69,123 @@ end
 
 %%% =====================================================================================
 %%%%% Figure 03
-figure('units','inches','outerposition',[7, 4, 4, 3.5])
+figure('units','inches','outerposition',[7, 4, 6, 4])
 
 set(gca,'ydir','reverse')
 hold on
 
-% clead = .4 * ones(1,3);
-clead = [59 128 238] / 256;
-% ctrail = .7 * ones(1,3);
-ctrail = [237 28 36] / 256;
-cerr = 'k';
-lw = 1.5;
-xtick_vec = -3:4;
-ytick_vec = 1:2;
-marker_sz = 20;
-barw = .4;
+c = lines(7);
+clead = c(1,:);
+ctrail = c(2,:);
+cfill1 = zeros(1,3);
+cfill2 = c(5,:) * .85;
+fill_alpha = .15;
+xtick_vec = -5:6;
+ytick_vec = [1.5, 5.5];
 
-% plot the leading mislocalizations
-x = [1, 2]+.25;
-ymat = repmat(x, nsub, 1);
-barh(x,xlead, barw, ...
-    'facecolor',clead, ...
-    'edgecolor','none')
-scatter( ...
-    [lead1 lead2], ymat, ...
-    marker_sz, 'k', 'o');
-errorbar(...
-    xlead, x, neglead, poslead,...
-    'o', ...
-    'horizontal', ...
-    'marker','none', ...    
-    'color',cerr, ...
-    'linewidth',lw)
+cmap = [ctrail;clead;ctrail;clead];
+scatterbar({trail1, lead1, trail2, lead2},cmap)
 
+xline(0, 'linestyle','--')
 
-% plot the trailing mislocalizations
-x = [1, 2]-.25;
-ymat = repmat(x, nsub, 1);
-barh(x,xtrail, barw, ...
-    'facecolor',ctrail, ...
-    'edgecolor','none')
-scatter( ...
-    [trail1 trail2], ymat, ...
-    marker_sz, 'k', 'o');
-errorbar(...
-    xtrail, x, negtrail, postrail,...
-    'o', ...
-    'horizontal', ...
-    'marker','none', ...    
-    'color',cerr, ...
-    'linewidth',lw)
-
-
-xlabel({'Perceived offset (dva)', '(in direction of motion)'})
-xticks(xtick_vec)
-xlim([xtick_vec(1)-.5 xtick_vec(end)+.5])
+xlabel({'Click offset (dva)', '(in direction of motion)'})
+xticks(-4:5)
+xlim([xtick_vec(1) xtick_vec(end)])
 
 yticks(ytick_vec)
-yticklabels({'One-probe', 'Two-probe'})
-ylim([ytick_vec(1)-.8 ytick_vec(end)+.6])
+set(gca,'YColor','none')
+ylim([0 6])
 
-text(-.5,.3,'Trailing probe','color',ctrail,'horizontalalignment','right')
-text(.5,.3,'Leading probe','color',clead,'horizontalalignment','left')
+text(-4.8, 1.5, 'One-probe', 'color', cfill1, 'horizontalalignment','right')
+text(-4.8, 4.5, 'Two-probe', 'color', cfill2, 'horizontalalignment','right')
+
+text(-.25,-.35,'Trailing probe','color',ctrail,'horizontalalignment','right')
+text(.25,-.35,'Leading probe','color',clead,'horizontalalignment','left')
+
+offset = .15;
+
+fill_obj = fill([-4 5.5 5.5 -4], [0+offset 0+offset 3-offset 3-offset], cfill1);
+fill_obj.LineWidth = 3;
+fill_obj.FaceAlpha = fill_alpha;
+fill_obj.EdgeColor = 'none';
+
+fill_obj = fill([-4 5.5 5.5 -4], [3+offset 3+offset 6-offset 6-offset], cfill2);
+fill_obj.LineWidth = 3;
+fill_obj.FaceAlpha = fill_alpha;
+fill_obj.EdgeColor = 'none';
 
 cleanplot
-
-saveas(gcf, '../results/fig02B.pdf')
 
 %%% ---------------------------------
 %%% stat fig 3
 
 %%% all vs baseline
-% 
-% disp([ ...
-%     'One-probe trail: ', ...
-%     'diff = ', num2str(mean(trail1)),' dva, ', ...
-%     'p = ', num2str(ptrail1) ...
-%     ])
-% 
-% disp([ ...
-%     'One-probe lead: ', ...
-%     'diff = ', num2str(mean(lead1)),' dva, ', ...
-%     'p = ', num2str(plead1) ...
-%     ])
-% 
-% disp([ ...
-%     'Two-probe trail: '...
-%     'diff = ', num2str(mean(trail2)),' dva, ', ...
-%     'p = ', num2str(ptrail2) ...
-%     ])
-% 
-% disp([ ...
-%     'Two-probe lead: ', ...
-%     'diff = ', num2str(mean(lead2)),' dva, ', ...
-%     'p = ', num2str(plead2) ...
-%     ])
-% 
-% disp('---')
-% 
-% %%% trail1 vs trail2
-% 
-% disp([ ...    
-%     'One-probe vs Two-probe trails: ', ...
-%     'diff = ', num2str(mean(trail2-trail1)), ' dva, ', ...
-%     'p = ', num2str(pdiff_trail) ...
-%     ])
-% 
-% 
-% %%% lead1 vs lead2
-% 
-% disp([ ...
-%     'One-probe vs Two-probe leads: ', ...
-%     'diff = ', num2str(mean(lead2-lead1)), ' dva; ', ...
-%     'p = ', num2str(pdiff_lead) ...
-%     ])
+[med, p, W, z, r] = signrank_full(trail1);
+fprintf('<trail1> md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
+
+[med, p, W, z, r] = signrank_full(lead1);
+fprintf('<lead1 > md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
+
+[med, p, W, z, r] = signrank_full(trail2);
+fprintf('<trail2> md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
+
+[med, p, W, z, r] = signrank_full(lead2);
+fprintf('<lead2 > md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n\n', ...
+med,W,z,p,r)
+
+text(-3.5, 1, '\it n.s.', 'color', 'k', 'horizontalalignment','center')
+text(5, 2, '***', 'fontsize', 18, 'color', 'k', 'horizontalalignment','center')
+text(-3.5, 4, '*', 'fontsize', 18, 'color', 'k', 'horizontalalignment','center')
+text(5, 5, '***', 'fontsize', 18, 'color', 'k', 'horizontalalignment','center')
+
+%%% trail1 vs trail2
+[med, p, W, z, r] = signrank_full(trail2,trail1);
+fprintf('<trail1 vs trail2> md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
+
+line([-4.2 -4.2], [1 4], 'color', 'k', 'linewidth', 2)
+text(-4.5, 2.5, '*', 'fontsize', 18, 'color', 'k', 'horizontalalignment','center')
+
+%%% lead1 vs lead2
+[med, p, W, z, r] = signrank_full(lead2,lead1);
+fprintf('<lead1  vs lead2 > md = %4.1f dva, W = %5d, z = %5.2f, p = %5.3f, r = %4.2f \n', ...
+med,W,z,p,r)
+
+line([5.7 5.7], [2 5], 'color', 'k', 'linewidth', 2)
+text(6, 3.5, '*', 'fontsize', 18, 'color', 'k', 'horizontalalignment','center')
+
+saveas(gcf, '../results/fig03.pdf')
+
+
+function scatterbar(A,c)
+% A: a cell of cetegories
+
+ncat    = numel(A); % number of categories
+stdx    = .05; % standard deviation of scatters in each category
+linelm  = .4; % line length for median
+marksz = 50;
+alpha = .7;
+
+hold on
+for icat = 1:ncat    
+    rng default
+    n = numel(A{icat});
+    
+    offset = icat;
+    if icat >2
+        offset = offset+1;
+    end
+
+    y = randn(n,1)*stdx + offset;
+    
+    scatter(A{icat},y, ...
+        marksz, c(icat,:), 'o', 'filled', 'markerfacealpha', alpha);
+    line([nanmedian(A{icat}) nanmedian(A{icat})],...
+        [offset-linelm offset+linelm], ...
+        'color',c(icat,:),'linewidth',1);
+end
+
+end
